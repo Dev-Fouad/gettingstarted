dc12e9211ca6f1f0a8d019ccc3af382262ed730c
fouad ajayi <fouadajayi6@gmail.com>
2024-05-19 23:51:14 +0100

    JAVA

--- src/main/java/Objectoriented.java
+++ src/main/java/Objectoriented.java
@@ -1,5 +1,5 @@
 public class Objectoriented {
     public static void main(String[] args) {
-        System.out.println("Java is an object oriented programing language, which is a programming based on the concept of objects, which may contain data");
+        System.out.println("Java is object oriented programming");
     }
 }
